0c3456669a9f0b8f1af161edf3fdded4f4803f98
Adrian Corcoran <adrian.corcoran@shopify.com>
2020-06-28 19:25:27 +0100

    Run Rubocop and fix linting

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 Monorail.configure do |config|
   config.registry = Monorail::FileRegistry
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -24,26 +25,26 @@ Item.create!(
       title: "Martian Chronicles",
       description: "Cult book by Ray Bradbury",
       user: john,
-      image_url: "https://upload.wikimedia.org/wikipedia/en/4/45/The-Martian-Chronicles.jpg"
+      image_url: "https://upload.wikimedia.org/wikipedia/en/4/45/The-Martian-Chronicles.jpg",
     },
     {
       title: "The Martian",
       description: "Novel by Andy Weir about an astronaut stranded on Mars trying to survive",
       user: john,
-      image_url: "https://upload.wikimedia.org/wikipedia/en/c/c3/The_Martian_2014.jpg"
+      image_url: "https://upload.wikimedia.org/wikipedia/en/c/c3/The_Martian_2014.jpg",
     },
     {
       title: "Doom",
       description: "A group of Marines is sent to the red planet via an ancient " \
                    "Martian portal called the Ark to deal with an outbreak of a mutagenic virus",
       user: jane,
-      image_url: "https://upload.wikimedia.org/wikipedia/en/5/57/Doom_cover_art.jpg"
+      image_url: "https://upload.wikimedia.org/wikipedia/en/5/57/Doom_cover_art.jpg",
     },
     {
       title: "Mars Attacks!",
       description: "Earth is invaded by Martians with unbeatable weapons and a cruel sense of humor",
       user: jane,
-      image_url: "https://upload.wikimedia.org/wikipedia/en/b/bd/Mars_attacks_ver1.jpg"
-    }
+      image_url: "https://upload.wikimedia.org/wikipedia/en/b/bd/Mars_attacks_ver1.jpg",
+    },
   ]
 )
